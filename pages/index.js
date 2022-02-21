85b8fefd9a23cc25c42d667b434163a14a74f922
apoorwa1 <apoorwa.saxena@gmail.com>
2022-02-21 10:12:29 +0000

    components folder

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-   <h1> Food Story </h1>
+      <h1> Food Story </h1>
+      <h3>User Login</h3>
 
- <Link href='/searchRecipe'><a>Search</a></Link>
- <br></br>
- <Link href='/createRecipe'><a>Create</a></Link>
-
-   </>
-  )
+      <Link href="/home">
+        <a>Search</a>
+      </Link>
+      <br></br>
+      <Link href="/createRecipe">
+        <a>Create</a>
+      </Link>
+    </>
+  );
 }
