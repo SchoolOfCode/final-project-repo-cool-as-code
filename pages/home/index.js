ab188d905bdc4210b0530e44db1dd53bd2277f6c
waynethrower <wayne.thrower@hotmail.co.uk>
2022-02-21 11:40:50 +0000

    Installed Antd for components

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import "antd/dist/antd.css";
 
 const SearchPage = () => {
-    return (
-        <div>
-           <h1>Search Page</h1> 
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Search Page</h1>
+    </div>
+  );
+};
 
-export default SearchPage
+export default SearchPage;
