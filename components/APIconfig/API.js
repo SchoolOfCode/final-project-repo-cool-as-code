c72340921784a9656da7b76b3f4ef53c023b686f
priya <pmistry1707@gmail.com>
2022-02-23 12:28:58 +0000

    FE & BE talking

--- components/APIconfig/API.js
+++ components/APIconfig/API.js
@@ -1,13 +1,13 @@
-import { API_URL, API_KEY, SEARCH_BASE_URL } from "../APIconfig/config";
+import { API_URL, SEARCH_BASE_URL } from "../APIconfig/config";
 
 const apiSettings = {
-  fetchRecipes: async (searchTerm) => {
-    const endpoint = searchTerm`${SEARCH_BASE_URL}${searchTerm}`;
-    return await (await fetch(endpoint)).json();
-  },
-  fetchRecipe: async (recipeId) => {
-    const endpoint = `${API_URL}recipes/${recipeId}`;
-    return await (await fetch(endpoint)).json();
-  },
+	fetchRecipes: async (searchTerm) => {
+		const endpoint = `${SEARCH_BASE_URL}${searchTerm}`;
+		return await (await fetch(endpoint)).json();
+	},
+	fetchRecipe: async (recipeId) => {
+		const endpoint = `${API_URL}recipes/${recipeId}`;
+		return await (await fetch(endpoint)).json();
+	},
 };
 export default apiSettings;
--- components/APIconfig/config.js
+++ components/APIconfig/config.js
@@ -2,8 +2,7 @@
 //Read more about the API: add link to API doc/read me
 
 const API_URL = "https://soc-foodstory.herokuapp.com/";
-const API_KEY = process.env.REACT_APP_API_KEY;
 
 const SEARCH_BASE_URL = `${API_URL}recipes?search=`;
 
-export { API_URL, API_KEY, SEARCH_BASE_URL };
+export { API_URL, SEARCH_BASE_URL };
