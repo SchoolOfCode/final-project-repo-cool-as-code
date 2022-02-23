73dda96485e75a22f9e4088f718b25ce0a6d77b0
apoorwa1 <apoorwa.saxena@gmail.com>
2022-02-23 09:51:00 +0000

    nextUI

--- components/RecipeCard/index.js
+++ components/RecipeCard/index.js
@@ -1,30 +1,63 @@
-import React from "react";
-import Image from "next/image";
-import RecipeCardCSS from "./RecipeCard.module.css";
-import "antd/dist/antd.css";
+// import React from "react";
+// import { Card } from "antd";
+// const { Meta } = Card;
+// import Image from "next/image";
+// import "./RecipeCard.module.css";
+// //import "antd/dist/antd.css";
 
-import { Card } from "antd";
-const { Meta } = Card;
+// //import { Card } from "antd";
+
+// function RecipeCard() {
+//   return (
+//     <>
+//       <Card
+//         hoverable
+//         style={{ width: 240 }}
+//         cover={
+//           <Image
+//             src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+//             width="240px"
+//             height="240px"
+//             alt="example"
+//           />
+//         }
+//       >
+//         <Meta title="Europe Street beat" description="www.instagram.com" />
+//       </Card>
+//       ,
+//       <div id="container" style="padding: 24px" />
+//     </>
+//   );
+// }
+
+import { Grid, Card, Row, Text } from "@nextui-org/react";
 
 function RecipeCard() {
   return (
-    <>
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={
-          <Image
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
-      ,
-      <div id="container" style="padding: 24px" />
-    </>
+    <Grid.Container gap={2} justify="flex-start">
+      <Grid xs={6} sm={3}>
+        <Card hoverable clickable shadow>
+          <Card.Body css={{ p: 0 }}>
+            <Card.Image
+              width="240px"
+              height="240px"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              alt="mytest"
+            />
+          </Card.Body>
+          <Card.Footer justify="flex-start">
+            <Row
+              wrap="wrap"
+              justify="space-between"
+              width="240px"
+              height="240px"
+            >
+              <Text b>Breakfast</Text>
+            </Row>
+          </Card.Footer>
+        </Card>
+      </Grid>
+    </Grid.Container>
   );
 }
-
 export default RecipeCard;
