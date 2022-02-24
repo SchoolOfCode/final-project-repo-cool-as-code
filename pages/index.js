ed3d5a21b73d5da976bc973f9e4f53d2f27545a9
priya <pmistry1707@gmail.com>
2022-02-24 19:28:53 +0000

    landing page progress :)

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,38 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
+import dynamic from "next/dynamic";
 
 import { Button } from "@nextui-org/react";
 import LoginButton from "../components/LoginButton/LoginButton";
+import { SubtitlesOutlined } from "@material-ui/icons";
+
+const BgImage = dynamic(
+	() => import("../components/BackgroundImage/BackgroundImage.js"),
+	{
+		ssr: false,
+	}
+);
 
 export default function Home() {
 	return (
 		<>
-			<Image
-				className={styles.landingImage}
-				src="/home.jpg"
-				alt="Photo of Food"
-				layout="fill"
-				objectFit="cover"
-				objectPosition="center"
-			/>
-			<Image
-				className={styles.landingLogo}
-				src="/foodstory.png"
-				alt="Photo of Food"
-				layout="responsive"
-				height="200px"
-				width="200px"
-				objectFit="cover"
-				objectPosition="center"
-			/>
-			<LoginButton
-				className={styles.landingButton}
-				color="gradient"
-				size="xl"
-				clickable="true"
-			>
-				ENTER
-			</LoginButton>
+			<div className={styles.wrapper}>
+				<Image
+					src="/image/foodstory_rev2.png"
+					alt="Foodstory Logo"
+					width="500%"
+					height="500%"
+				/>
+				<LoginButton
+					className={styles.landingButton}
+					color="gradient"
+					size="xl"
+					clickable="true"
+				>
+					ENTER
+				</LoginButton>
+			</div>
+			<BgImage />
 		</>
 	);
 }
