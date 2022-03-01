e1e2a7568b27b157b0ba8014e442f8b34544f80c
priya <pmistry1707@gmail.com>
2022-03-01 11:54:48 +0000

    POST wip

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,37 +1,136 @@
-import React from "react"
+import React, { useState } from "react";
 
 //styling
-import ButtonStyles from "./Button.module.css"
+import ButtonStyles from "./Button.module.css";
 
 //API
-import API from "../../components/APIconfig/API"
+// import API from "../../components/APIconfig/API";
 
-const post = { recipe: "test" }
-const API_URL = "https://recipe-soc-project.herokuapp.com/recipes"
+const API_URL = "https://recipe-soc-project.herokuapp.com/recipes";
 
-const Button = ({ onClick }) => {
-  async function createPosts(post) {
-    const response = await fetch(`${API_URL}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(post),
-    })
-    const data = await response.json()
-    console.log(data)
-  }
+function Button(props) {
+	const { addNewRecipe } = props;
 
-  function onClick() {
-    console.log("Hi")
-    createPosts(post)
-  }
+	const [title, setTitle] = useState("");
+	const [portions, setPortions] = useState("");
+	const [story, setStory] = useState("");
+	const [tags, setTags] = useState([]);
+	const [type, setType] = useState("");
+	const [ingredients, setIngredients] = useState([]);
+	const [instructions, setInstructions] = useState([]);
+	const [image, setImage] = useState("");
 
-  return (
-    <>
-      <button onClick={onClick} className={ButtonStyles.button}>
-        Button
-      </button>
-    </>
-  )
+	function handleChangeTitle(event) {
+		setTitle(event.target.value);
+	}
+
+	function handleChangePortions(event) {
+		setPortions(event.target.value);
+	}
+
+	function handleChangeStory(event) {
+		setStory(event.target.value);
+	}
+
+	function handleChangeTags(event) {
+		setTags(event.target.value);
+	}
+
+	function handleChangeType(event) {
+		setType(event.target.value);
+	}
+
+	function handleChangeIngredients(event) {
+		setIngredients(event.target.value);
+	}
+
+	function handleChangeInstructions(event) {
+		setInstructions(event.target.value);
+	}
+
+	function handleChangeImage(event) {
+		setImage(event.target.value);
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		const recipe = {
+			title,
+			portions,
+			story,
+			tags,
+			type,
+			ingredients,
+			instructions,
+			image,
+		};
+		console.log(recipe);
+		addNewRecipe(recipe);
+		resetState();
+	}
+
+	function resetState() {
+		setTitle("");
+		setPortions("");
+		setStory("");
+		setTags([]);
+		setType("");
+		setIngredients([]);
+		setInstructions([]);
+		setImage("");
+	}
+
+	return (
+		<form action="">
+			<input
+				type="text"
+				placeholder="Recipe Name"
+				onChange={handleChangeTitle}
+			/>
+			<br />
+			<input
+				type="number"
+				min="0"
+				placeholder="Number of Portions"
+				onChange={handleChangePortions}
+			/>
+			<br />
+			<input
+				type="text"
+				placeholder="Story Behind the Recipe"
+				onChange={handleChangeStory}
+			/>
+			<br />
+			<input type="text" placeholder="Enter Tags" onChange={handleChangeTags} />
+			<br />
+			<input type="text" placeholder="Enter Type" onChange={handleChangeType} />
+			<br />
+			<input
+				type="text"
+				placeholder="Enter Ingredients"
+				onChange={handleChangeIngredients}
+			/>
+			<br />
+			<input
+				type="text"
+				placeholder="Enter Instructions"
+				onChange={handleChangeInstructions}
+			/>
+			<br />
+			<input
+				type="text"
+				placeholder="Enter Image"
+				onChange={handleChangeImage}
+			/>
+			<button
+				type="submit"
+				onClick={handleSubmit}
+				className={ButtonStyles.button}
+			>
+				SAVE
+			</button>
+		</form>
+	);
 }
 
-export default Button
+export default Button;
--- pages/createRecipe/index.js
+++ pages/createRecipe/index.js
@@ -1,16 +1,27 @@
-import React from "react"
+import React from "react";
 
 //components
-import Button from "../../components/Button"
+import Button from "../../components/Button";
 
 const createRecipe = () => {
-  return (
-    <div>
-      <h1>Create Recipe</h1>
+	async function addNewRecipe(recipe) {
+		const response = await fetch(
+			"https://recipe-soc-project.herokuapp.com/recipes",
+			{
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(recipe),
+			}
+		);
+		const data = await response.json();
+		console.log(data);
+	}
+	return (
+		<div>
+			<h1>Create Recipe</h1>
+			<Button addNewRecipe={addNewRecipe} />
+		</div>
+	);
+};
 
-      <Button />
-    </div>
-  )
-}
-
-export default createRecipe
+export default createRecipe;
