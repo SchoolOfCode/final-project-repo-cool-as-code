3ef1cf95c8b7580c994790801912cd49feba548f
priya <pmistry1707@gmail.com>
2022-03-04 17:12:21 +0000

    small changes

--- pages/home/index.js
+++ pages/home/index.js
@@ -13,26 +13,26 @@ import MtButton from "../../components/MealTypeButton";
 import FeatureButton from "../../components/FeatureButton";
 
 const Home = () => {
-  return (
-    <>
-      <div>
-        <Header />
-        <h1>Home Page</h1>
+	return (
+		<>
+			<div>
+				<Header />
+				<h1>Home Page</h1>
 
-        <SearchInput />
-        <nav className={PageStyling.grid}>
-          <MtButton onClick={() => getMeal} mealTypes="Breakfast" />
-          <MtButton onClick={() => getMeal} mealTypes="Lunch" />
-          <MtButton onClick={() => getMeal} mealTypes="Dinner" />
-          <MtButton onClick={() => getMeal} mealTypes="Snack" />
-          <FeatureButton />
-          <FeatureButton />
-          <FeatureButton />
-          <FeatureButton />
-        </nav>
-      </div>
-    </>
-  );
+				<SearchInput />
+				<nav className={PageStyling.grid}>
+					<MtButton onClick={() => getMeal} mealTypes="Breakfast" />
+					<MtButton onClick={() => getMeal} mealTypes="Lunch" />
+					<MtButton onClick={() => getMeal} mealTypes="Dinner" />
+					<MtButton onClick={() => getMeal} mealTypes="Snack" />
+					<FeatureButton />
+					<FeatureButton />
+					<FeatureButton />
+					<FeatureButton />
+				</nav>
+			</div>
+		</>
+	);
 };
 
 export default Home;
