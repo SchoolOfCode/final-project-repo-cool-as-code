40cb8fb2e5418727334f7194a96626f02718dc73
waynethrower <wayne.thrower@hotmail.co.uk>
2022-03-04 14:55:50 +0000

    Recipe Display now showing some data

---
+++ components/RecipeDisplay/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Image from "next/image"
+
+//styles
+import Display from "./RecipeDisplay.module.css"
+
+function RecipeDisplay({ title, image, portions, story }) {
+  return (
+    <>
+      <div>RecipeDisplay</div>
+      <p>{title}</p>
+      <Image src={image} width={500} height={500} alt="Recipe Picture" />
+      <p>{portions}</p>
+      <p>{story}</p>
+    </>
+  )
+}
+
+export default RecipeDisplay
--- pages/showRecipe/index.js
+++ pages/showRecipe/index.js
@@ -1,45 +1,36 @@
-import React, { useEffect } from "react";
-import { useContext, useState } from "react";
-//import API from "../../components/APIconfig/API";
-import Image from "next/image";
+import React, { useEffect } from "react"
+import { useContext, useState } from "react"
+import Image from "next/image"
+import { pageWrapper } from "../../pages/_app"
 
-import { pageWrapper } from "../../pages/_app";
+//components
+import RecipeDisplay from "../../components/RecipeDisplay"
 
 const showRecipe = () => {
-  let { state, setState } = useContext(pageWrapper);
-  const [data, setData] = useState({});
-  // const fetchRecipes = async (recipeId) => {
-  //   const response = await API.fetchRecipe(recipeId);
-  //   setState({
-  //     ...state,
-  //     apiData: response.payload,
-  //     apiMessage: response.message,
-  //   });
-  //console.log(state);
-
-  //fetchRecipes();
+  let { state, setState } = useContext(pageWrapper)
 
   let recipe = state.apiData.filter(
     (item) => item.id === state.selectingRecipe.recipeId
-  );
-  useEffect(() => {
-    setData(recipe);
-  }, []);
-  console.log(data);
+  )
 
-  //setRecipe(recipe);
+  console.log(recipe)
+
   return (
     <div>
       <h1>Here is the recipe you were looking for</h1>
 
-      <>
-        <p>{data.title}</p>
-        {/* <Image src={recipe.image} layout="fill" alt="Recipe Picture" /> */}
-        <p>{data.portions}</p>
-        <p>{data.story}</p>
-      </>
+      {recipe.map((item, index) => (
+        <>
+          <RecipeDisplay
+            title={item.title}
+            image={item.image}
+            portions={item.portions}
+            story={item.story}
+          />
+        </>
+      ))}
     </div>
-  );
-};
+  )
+}
 
-export default showRecipe;
+export default showRecipe
