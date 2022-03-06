2345a7162c035e88c35881e5f7196a9fbb5a45fd
waynethrower <wayne.thrower@hotmail.co.uk>
2022-03-06 02:56:30 +0000

    Renamed Page to Recipe

--- pages/Recipe/index.js
+++ pages/Recipe/index.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react"
 import { useContext, useState } from "react"
 import Image from "next/image"
-import { pageWrapper } from "../../pages/_app"
+import { pageWrapper } from "../_app"
 
 //components
 import RecipeDisplay from "../../components/RecipeDisplay"
 import Template from "../template"
 
-const showRecipe = () => {
+const Recipe = () => {
   let { state, setState } = useContext(pageWrapper)
 
   let recipe = state.apiData.filter(
@@ -35,4 +35,4 @@ const showRecipe = () => {
   )
 }
 
-export default showRecipe
+export default Recipe
