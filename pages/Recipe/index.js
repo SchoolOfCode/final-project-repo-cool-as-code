4f23998a7bb45b303933e2e117087211de00bb0b
priya <pmistry1707@gmail.com>
2022-03-07 23:45:40 +0000

    delete recipe functionality complete

--- pages/Recipe/index.js
+++ pages/Recipe/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useContext, useState } from "react";
 // import Image from "next/image";
 import { pageWrapper } from "../_app";
@@ -8,12 +8,11 @@ import Header from "../../components/Header";
 import styles from "./styles.module.css";
 
 //components
-import RecipeDisplay from "../../components/RecipeDisplay";
-import Template from "../template";
 import RenderIngredients from "../../components/RenderIngredients";
 import RenderInstructions from "../../components/RenderInstructions";
 import RenderTags from "../../components/RenderTags";
 import RenderStory from "../../components/RenderStory";
+import DeleteRecipeButton from "../../components/DeleteRecipeButton";
 
 import {
 	Tabs,
@@ -24,7 +23,42 @@ import {
 	Text,
 	Stack,
 	Image,
+	Button,
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogContent,
+	AlertDialogOverlay,
+	Center,
+	Spinner,
 } from "@chakra-ui/react";
+
+/*
+let item = {
+	id: 72,
+	title: "Easy carrot cake",
+	portions: 10,
+	story:
+		"Top this classic carrot cake with moreish icing and chopped walnuts or pecans. Serve as a sweet treat with a cup of tea any time of the day.",
+	tags: ["Desserts", "British", "Vegetarian"],
+	type: "Dessert",
+	ingredients: [
+		'{"quantity":230,"measure":"ml ","food":"vegetable oil, plus extra for the tin"}',
+		'{"quantity":100,"measure":"g","food":"natural yogurt"}',
+		'{"quantity":4,"measure":"large","food":"eggs"}',
+		'{"quantity":1,"measure":"tsp ","food":"vanilla extract"}',
+	],
+	instructions: [
+		'{"instruction":"Heat the oven to 180C/160C fan/gas 4. Oil and line the base and sides of two 20cm cake tins with baking parchment. Whisk the oil, yogurt, eggs, vanilla and zest in a jug. Mix the flour, sugar, cinnamon and nutmeg with a good pinch of salt in a bowl. Squeeze any lumps of sugar through your fingers, shaking the bowl a few times to bring the lumps to the surface.","image":""}',
+		'{"instruction":"Add the wet ingredients to the dry, along with the carrots, raisins and half the nuts, if using. Mix well to combine, then divide between the tins.","image":""}',
+		'{"instruction":"Bake for 25-30 mins or until a skewer inserted into the centre of the cake comes out clean. If any wet mixture clings to the skewer, return to the oven for 5 mins, then check again. Leave to cool in the tins.","image":""}',
+		'{"instruction":"To make the icing, beat the butter and sugar together until smooth. Add half the soft cheese and beat again, then add the rest (adding it bit by bit prevents the icing from splitting). Remove the cakes from the tins and sandwich together with half the icing. Top with the remaining icing and scatter with the remaining walnuts. Will keep in the fridge for up to five days. Best eaten at room temperature.","image":""}',
+	],
+	image:
+		"https://res.cloudinary.com/dia4he3t2/image/upload/v1646676636/kt8sgi00htgxbfroxzuu.webp",
+};
+*/
 
 //conditional statments to render the data
 //To render :
@@ -40,7 +74,30 @@ import {
 // render each instruction on a line
 // render image too
 
+const API_URL = process.env.API_URL;
+
 const recipe = () => {
+	const router = useRouter();
+	const [deleteIsOpen, setDeleteIsOpen] = useState(false);
+	const onDeleteClose = () => setDeleteIsOpen(false);
+	const cancelDeleteRef = useRef();
+
+	const [recipeDelete, setRecipeDelete] = useState(false);
+
+	async function handleDeleteRecipe(id) {
+		setRecipeDelete(true);
+		console.log("delete the recipe", id);
+		const response = await fetch(`${API_URL}/recipes/${id}`, {
+			method: "DELETE",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(),
+		});
+		const data = await response.json();
+		console.log(data, "recipe deleted");
+		onDeleteClose;
+		router.push("/home");
+	}
+
 	let { state, setState } = useContext(pageWrapper);
 
 	let recipe = state.apiData.filter(
@@ -57,7 +114,6 @@ const recipe = () => {
 		router.push("/home");
 	}
 
-	const router = useRouter();
 	function setId() {
 		setState({ ...state, selectingRecipe: { recipeId } });
 		router.push("/recipe");
@@ -66,7 +122,7 @@ const recipe = () => {
 	return (
 		<div>
 			{recipe.map((item, index) => (
-				<div>
+				<div key={index}>
 					<Header />
 					<h1 className={styles.title}>{item.title}</h1>
 					<div className={styles.createRecipe}>
@@ -155,30 +211,63 @@ const recipe = () => {
 										Story
 									</Tab>
 								</TabList>
-								<TabPanels>
-									<TabPanel>
-										{item.ingredients.length !== 0 ? (
-											<RenderIngredients ingredients={item.ingredients} />
-										) : (
-											<Text>
-												You have not entered any ingredients for this recipe
-											</Text>
-										)}
-									</TabPanel>
-									<TabPanel>
-										{item.instructions.length !== 0 ? (
-											<RenderInstructions instructions={item.instructions} />
-										) : (
-											<Text>
-												You have not entered the method for this recipe
-											</Text>
-										)}
-									</TabPanel>
-									<TabPanel>
-										<RenderStory story={item.story} />
-									</TabPanel>
-								</TabPanels>
+								{/* <TabPanels>
+								<TabPanel>
+									{item.ingredients.length !== 0 ? (
+										<RenderIngredients ingredients={item.ingredients} />
+									) : (
+										<Text>
+											You have not entered any ingredients for this recipe
+										</Text>
+									)}
+								</TabPanel>
+								<TabPanel>
+									{item.instructions.length !== 0 ? (
+										<RenderInstructions instructions={item.instructions} />
+									) : (
+										<Text>You have not entered the method for this recipe</Text>
+									)}
+								</TabPanel>
+								<TabPanel>
+									<RenderStory story={item.story} />
+								</TabPanel>
+							</TabPanels> */}
 							</Tabs>
+							<div className={styles.buttonsContainer}>
+								<div className={styles.buttonDiv}>
+									<DeleteRecipeButton
+										setDeleteIsOpen={setDeleteIsOpen}
+										deleteIsOpen={deleteIsOpen}
+										cancelDeleteRef={cancelDeleteRef}
+										onDeleteClose={onDeleteClose}
+										recipeDelete={recipeDelete}
+										handleDeleteRecipe={handleDeleteRecipe}
+										item={item}
+									/>
+								</div>
+								<div className={styles.buttonDiv}>
+									<Button
+										border="1px"
+										bg="orange.main"
+										borderRadius="8px"
+										borderColor="orange.main"
+										color="blue.main"
+										size="lg"
+										_hover={{ bg: "orange.one" }}
+										_active={{
+											bg: "orange.one",
+											transform: "scale(0.98)",
+											borderColor: "orange.one",
+										}}
+										_focus={{
+											boxShadow:
+												"0 0 1px 2px orange.one, 0 1px 1px orange.main",
+										}}
+									>
+										EDIT
+									</Button>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
