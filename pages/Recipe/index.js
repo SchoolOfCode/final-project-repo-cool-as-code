6aa339148a6862e49b506550f93ff8889cb43832
apoorwa1 <apoorwa.saxena@gmail.com>
2022-03-07 14:07:35 +0000

    recipe page wip

--- pages/Recipe/index.js
+++ pages/Recipe/index.js
@@ -3,6 +3,9 @@ import { useContext, useState } from "react";
 import Image from "next/image";
 import { pageWrapper } from "../_app";
 
+import Header from "../../components/Header";
+import styles from "./styles.module.css";
+
 //components
 import RecipeDisplay from "../../components/RecipeDisplay";
 import Template from "../template";
@@ -18,11 +21,8 @@ const recipe = () => {
 
   return (
     <div>
-      <h1>Here is the recipe you were looking for</h1>
-
       {recipe.map((item, index) => (
         <>
-          <Template title={item.title} />
           {/* <RecipeDisplay
             title={item.title}
             image={item.image}
@@ -31,6 +31,99 @@ const recipe = () => {
           /> */}
         </>
       ))}
+      <div>
+        <Header />
+        <h1 className={styles.title}>{item.title}</h1>
+        <div className={styles.createRecipe}>
+          <div className={styles.recipeForm}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            optio possimus veritatis asperiores est! Nostrum temporibus corporis
+            eum minima? Culpa tenetur pariatur quisquam unde atque? Quisquam,
+            iure! Atque, ab ducimus? Libero illo incidunt, magnam optio animi
+            ullam quae non veniam, enim, nemo provident necessitatibus fugit
+            quidem debitis cum perferendis odit? In optio praesentium eius
+            voluptate officiis perspiciatis expedita autem recusandae quos,
+            deserunt ex ut harum cupiditate dolorum atque sequi rerum libero
+            inventore accusamus debitis blanditiis delectus. Numquam nam
+            expedita modi est labore. Dicta illo vero odit possimus, ad libero
+            aspernatur, neque, eveniet deserunt sint error vel dignissimos.
+            Nesciunt, laudantium nisi?
+          </div>
+          <br />
+          <div className={styles.tabContainer}>
+            <Tabs className={styles.tabs} isFitted variant="enclosed">
+              <TabList mb="1em">
+                <Tab
+                  border="1px"
+                  bg="#ffe6c8"
+                  borderColor="#fbd3a2"
+                  _selected={{ color: "#023047", bg: "#fb8500" }}
+                  _active={{
+                    bg: "rgb(255, 226, 146)",
+                    transform: "scale(0.98)",
+                    borderColor: "#fb8500",
+                  }}
+                  _focus={{
+                    boxShadow:
+                      "0 0 1px 2px #fb8500, 0 1px 1px rgba(0, 0, 0, .15)",
+                  }}
+                >
+                  Ingredients
+                </Tab>
+                <Tab
+                  bg="#ffe6c8"
+                  borderColor="#fbd3a2"
+                  _selected={{ color: "#023047", bg: "#fb8500" }}
+                  _active={{
+                    bg: "rgb(255, 226, 146)",
+                    transform: "scale(0.98)",
+                    borderColor: "#fb8500",
+                  }}
+                  _focus={{
+                    boxShadow:
+                      "0 0 1px 2px #fb8500, 0 1px 1px rgba(0, 0, 0, .15)",
+                  }}
+                >
+                  Method
+                </Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Officia optio possimus veritatis asperiores est! Nostrum
+                  temporibus corporis eum minima? Culpa tenetur pariatur
+                  quisquam unde atque? Quisquam, iure! Atque, ab ducimus? Libero
+                  illo incidunt, magnam optio animi ullam quae non veniam, enim,
+                  nemo provident necessitatibus fugit quidem debitis cum
+                  perferendis odit? In optio praesentium eius voluptate officiis
+                  perspiciatis expedita autem recusandae quos, deserunt ex ut
+                  harum cupiditate dolorum atque sequi rerum libero inventore
+                  accusamus debitis blanditiis delectus. Numquam nam expedita
+                  modi est labore. Dicta illo vero odit possimus, ad libero
+                  aspernatur, neque, eveniet deserunt sint error vel
+                  dignissimos. Nesciunt, laudantium nisi?
+                </TabPanel>
+                <TabPanel>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptas, temporibus porro velit iste perspiciatis odio
+                  reiciendis autem minus, tenetur alias, ab mollitia aliquid.
+                  Magnam consectetur numquam quasi dolor omnis, quos quidem
+                  laudantium eaque quia doloribus, aliquam nam, perferendis
+                  quaerat sint minus non! Nemo ducimus exercitationem excepturi
+                  sed temporibus! Minus deleniti et itaque dolorem a consequatur
+                  iure nesciunt dolore autem. Nulla architecto neque magnam,
+                  enim voluptatibus saepe quo exercitationem perspiciatis
+                  numquam consectetur harum libero, doloribus culpa maxime
+                  magni, mollitia quam assumenda quia! Laboriosam impedit
+                  assumenda recusandae laborum doloribus aperiam voluptate eum
+                  amet ad ipsam tempora quidem inventore necessitatibus labore,
+                  voluptas commodi!
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
