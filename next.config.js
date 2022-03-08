0baea2a85e6c4a28ff616441dfb1d5a774d866c0
apoorwa1 <apoorwa.saxena@gmail.com>
2022-03-08 11:20:06 +0000

    cypress installed

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
 
 module.exports = {
-
-
-	images: {
-		domains: ["https://i.ibb.co", "images.immediate.co.uk"],
-	},
-	env: {
-		API_URL: "https://recipe-soc-project.herokuapp.com",
-	},
+  images: {
+    domains: ["https://i.ibb.co", "images.immediate.co.uk"],
+  },
+  env: {
+    API_URL: "https://recipe-soc-project.herokuapp.com",
+  },
+  future: {
+    webpack5: true,
+  },
 };
-
