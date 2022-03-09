a48a4267e6a709af5eb79e23c5c541162407105b
apoorwa1 <apoorwa.saxena@gmail.com>
2022-03-09 15:13:14 +0000

    recipecard wip

--- components/RecipeCard/index.js
+++ components/RecipeCard/index.js
@@ -1,33 +1,30 @@
-
-import React, { useState, useContext } from "react"
-import Image from "next/image"
-import { useRouter } from "next/router"
+import React, { useState, useContext } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Card, Grid, Row, Text } from "@nextui-org/react";
 
 //if no image exists
-import errImg from "../../public/image/no_image.jpg"
-
-
+import errImg from "../../public/image/no_image.jpg";
 
 //styles
 import CardStyles from "./RecipeCard.module.css";
 import { pageWrapper } from "../../pages/_app";
 
 function RecipeCard({ recipeId, image, recipeName, title, p1, p2 }) {
-
-  let { state, setState } = useContext(pageWrapper)
-  const [imgSrc, setImgSrc] = useState(image)
-  const router = useRouter()
+  let { state, setState } = useContext(pageWrapper);
+  const [imgSrc, setImgSrc] = useState(image);
+  const router = useRouter();
   function setId() {
-    setState({ ...state, selectingRecipe: { recipeId } })
-    router.push("/Recipe")
+    setState({ ...state, selectingRecipe: { recipeId } });
+    router.push("/Recipe");
   }
   const handleOnError = () => {
     // console.log("error on:", recipeId)
-    setImgSrc(errImg)
-  }
+    setImgSrc(errImg);
+  };
   return (
     <>
-      <div>
+      {/* <div>
         <div className={CardStyles.flip_card}>
           <div className={CardStyles.flip_card_inner}>
             <div className={CardStyles.flip_card_front}>
@@ -51,9 +48,33 @@ function RecipeCard({ recipeId, image, recipeName, title, p1, p2 }) {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
+      {/* <Grid.Container gap={2} justify="flex-start">
+        {list.map((item, index) => ( */}
+      {/* <Grid xs={6} sm={3} key={index}> */}
+      <Card hoverable clickable>
+        <Card.Body css={{ p: 0 }}>
+          <Card.Image
+            objectFit="cover"
+            src={imgSrc.length > 0 || imgSrc === null ? imgSrc : errImg}
+            width="100%"
+            height={140}
+            alt={title}
+          />
+        </Card.Body>
+        <Card.Footer justify="flex-start">
+          <Row wrap="wrap" justify="space-between">
+            <Text b>{recipeName}</Text>
+            <Text css={{ color: "$accents4", fontWeight: "$semibold" }}>
+              {title}
+            </Text>
+          </Row>
+        </Card.Footer>
+      </Card>
+      {/* </Grid> */}
+      {/* ))}
+      </Grid.Container> */}
     </>
-  )
-
+  );
 }
 export default RecipeCard;
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,63 +1,64 @@
-import React, { useState, useEffect, useContext } from "react"
+import React, { useState, useEffect, useContext } from "react";
 import { useUser } from "@auth0/nextjs-auth0";
 
-import { pageWrapper } from "../_app"
+import { pageWrapper } from "../_app";
 import FadeIn from "react-fade-in/lib/FadeIn";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { Grid } from "@nextui-org/react";
 
 //styling
-import css from "../home/Home.module.css"
+import css from "../home/Home.module.css";
 
 //components
-import Header from "../../components/Header"
-import MtButton from "../../components/MealTypeButton"
-import Grid from "../../components/Grid"
-import RecipeCard from "../../components/RecipeCard"
+import Header from "../../components/Header";
+import MtButton from "../../components/MealTypeButton";
+//import Grid from "../../components/Grid";
+import RecipeCard from "../../components/RecipeCard";
 
 //API
-import API from "../../components/APIconfig/API"
-import SearchBarWrapper from "../../components/SearchBarWrapper"
+import API from "../../components/APIconfig/API";
+import SearchBarWrapper from "../../components/SearchBarWrapper";
 
 const Home = () => {
-  let { state, setState } = useContext(pageWrapper)
-  let [initial, setInitial] = useState(false)
-  let [results, setResults] = useState(false)
+  let { state, setState } = useContext(pageWrapper);
+  let [initial, setInitial] = useState(false);
+  let [results, setResults] = useState(false);
 
   //useUser from Auth) to get user
   const { user, error, isLoading } = useUser();
-  
-console.log(user)
+
+  console.log(user);
   //useEffect for fecthing All recipes
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await API.fetchAllRecipes()
+        const response = await API.fetchAllRecipes();
         setState({
           ...state,
           apiData: response.payload,
           apiMessage: response.message,
-        })
-        setInitial(response.payload)
+        });
+        setInitial(response.payload);
       } catch (error) {
         //set error
       }
-    }
-    fetchData()
-  }, [])
+    };
+    fetchData();
+  }, []);
 
   // useEffect for Search functionality
   useEffect(() => {
     //console.log(results, state.searchResult)
-    setResults(state.searchResult)
+    setResults(state.searchResult);
     //console.log("check state results:", state, "check results:", results)
-  }, [state.searchMessage])
+  }, [state.searchMessage]);
 
   return (
     <>
       <Header />
       <FadeIn transitionDuration={500} delay={200}>
-      <h1 className={css.title}>What do you fancy?</h1>
-    </FadeIn>
+        <h1 className={css.title}>What do you fancy?</h1>
+      </FadeIn>
       {/* <SearchInput /> */}
       <SearchBarWrapper />
       <div className={css.background}>
@@ -73,23 +74,23 @@ console.log(user)
         {results &&
           (console.log(!state.searchMessage.length),
           (
-            <Grid
-              header={`${results.length} Search Results: ${state.searchMessage}`}
-            >
+            <Grid.Container gap={2} justify="flex-start" display="grid">
               {console.log(state.searchResult, state.searchMessage)}
               {results.length > 0 &&
                 results.map((item, index) => (
-                  <RecipeCard
-                    key={index}
-                    recipeId={item.id}
-                    image={item.image}
-                    recipeName={item.title}
-                    title={item.story}
-                    p1={item.type}
-                    p2={item.tags}
-                  />
+                  <Grid xs={6} sm={3} key={index}>
+                    <RecipeCard
+                      key={index}
+                      recipeId={item.id}
+                      image={item.image}
+                      recipeName={item.title}
+                      title={item.story}
+                      p1={item.type}
+                      p2={item.tags}
+                    />
+                  </Grid>
                 ))}
-            </Grid>
+            </Grid.Container>
           ))}
 
         {/* {initial != undefined && console.log(state.apiData, initial)} */}
@@ -109,14 +110,14 @@ console.log(user)
                     p1={item.type}
                     p2={item.tags}
                   />
-                )
+                );
               })}
           </Grid>
         )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 //export const getServerSideProps = withPageAuthRequired();
